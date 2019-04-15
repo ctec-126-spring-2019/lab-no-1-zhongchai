7aaacee46f1011b4943f937d6f7951c7e5ec8a30
belgort-clark <belgort@clark.edu>
2019-04-14 18:21:16 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ctec-126-lab-1
+# CTEC 126: Lab 1 - Part 1
+
+## Details
+
+For this portion of the lab you are to fix any and all HTML and JavaScript syntax errors.
+
+The two files that you need to look at are:
+
+- lab1-part1.html
+- js/lab1.js
+
+## Grading Rubric
+
+There a total of 10 errors and fixing each error is worth 10 points for a total of 100 points.
---
+++ js/lab1-part1.js
@@ -0,0 +1,33 @@
+// lab1-part1.js
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+function listSkills(){
+    let skills = ['Web Development','Gardening','Drumming','Volunteering';
+    let output = "<h2>Bruce's Skills<h2>";
+    output += '<ul>';
+    for (let i = 0; i < skills.length; i++) {
+        output += '<li>' + skills[i] + '</li>';
+    }
+    output += '</ul>';
+    document.getElementById('skills').innerHTML = '<p>' + output + '</p>';
+}
+
+function getStarted(){
+    let firstName  'Bruce'
+    let lastName = 'Elgort';
+    let collegeName = 'Clark College'
+    let city = 'Vancouver, Washington';
+    let gpa = 3.2;
+    let output = `${firstName} {lastName} is currently attending ${collegeName}, located in {city} and has a grade point average of {gpa}`;
+
+    document.getElementById('place1').innerHTML = '<p>' + output + '</p>";
+    document.getElementById('place2').innerHTML = '<p>' + reverseString(output) + '</p>';
+}
+
+window.onload = function(){
+    getStarted();
+    listSkills();
+}
