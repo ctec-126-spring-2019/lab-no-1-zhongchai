abbcbe2dc8abd897698eb7679052f29eca96cccc
zhongchai <z.chai@students.clark.edu>
2019-04-16 22:02:24 -0700

    fixed errors

--- js/lab1.js
+++ js/lab1.js
@@ -5,8 +5,10 @@ function reverseString(str) {
 }
 
 function listSkills(){
-    let skills = ['Web Development','Gardening','Drumming','Volunteering';
-    let output = "<h2>Bruce's Skills<h2>";
+    // missing closing square bracket
+    let skills = ['Web Development','Gardening','Drumming','Volunteering'];
+    // <h2> tag wasn't closed properly
+    let output = "<h2>Bruce's Skills</h2>";
     output += '<ul>';
     for (let i = 0; i < skills.length; i++) {
         output += '<li>' + skills[i] + '</li>';
@@ -16,14 +18,18 @@ function listSkills(){
 }
 
 function getStarted(){
-    let firstName  'Bruce'
+    // missing '=' sign and semicolon (2 errors)
+    let firstName = 'Bruce';
     let lastName = 'Elgort';
-    let collegeName = 'Clark College'
+    // missing semicolon (this should still work but should be added for consistency's sake)
+    let collegeName = 'Clark College';
     let city = 'Vancouver, Washington';
     let gpa = 3.2;
-    let output = `${firstName} {lastName} is currently attending ${collegeName}, located in {city} and has a grade point average of {gpa}`;
+    // the dollar sign before the lastName, city, and gpa variables are missing (3 errors)
+    let output = `${firstName} ${lastName} is currently attending ${collegeName}, located in ${city} and has a grade point average of ${gpa}`;
 
-    document.getElementById('place1').innerHTML = '<p>' + output + '</p>";
+    // the quotation marks around the </p> do not match (one side is single and the other is double)
+    document.getElementById('place1').innerHTML = '<p>' + output + '</p>';
     document.getElementById('place2').innerHTML = '<p>' + reverseString(output) + '</p>';
 }
 
